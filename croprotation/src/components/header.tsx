e18e80e47e31c22b76b0e1474ac8164b18103082
j <giorgosmelas2002@gmail.com>
2025-03-15 16:30:56 +0200

    Start creating header

---
+++ croprotation/src/components/header.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import style from "../styling/header.module.css";
+
+const Header = () => {
+  return (
+    <header className={style.header}>
+
+    </header>
+  );
+};
+
+export default Header;
