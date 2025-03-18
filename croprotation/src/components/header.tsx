709aa76926d49e51a4bebec614488fc2c0b44c42
giorgosmelas2 <giorgosmelas2002@gmail.com>
2025-03-18 19:50:01 +0200

    Fixed header texr problems.

--- croprotation/src/components/header.tsx
+++ croprotation/src/components/header.tsx
@@ -5,7 +5,16 @@ import style from "../styling/header.module.css";
 const Header = () => {
   return (
     <header className={style.header}>
+      <div className={style.leftSection}>
+        <img className={style.logo} src="/crop-rotation-color-icon-ok.png" alt="Crop Rotation Logo" />
+        <h1>Crop Rotation</h1>
+      </div>
 
+      <div className={style.rigthSection}>
+        <button>About us</button>
+        <button>Rotation Plan</button>
+        <button> Login</button>
+      </div>
     </header>
   );
 };
