e2b8705d832abd79af91775fbcc64e216c8de2ee
giorgosmelas2 <giorgosmelas2002@gmail.com>
2025-03-27 19:26:54 +0200

    Half ready home page

--- croprotation/src/pages/home.tsx
+++ croprotation/src/pages/home.tsx
@@ -1,16 +1,28 @@
 import React from "react"
+import style from "../styling/home.module.css"
 
 const Home = () => {
-    return (<div>
-        <h3>AI-Powered Crop Rotation Advisor for Farmers
+    return (
+        <div>
 
-            Welcome to our AI-driven crop rotation recommendation system, designed to help farmers optimize their planting strategies for healthier soil, increased yields, and greater profitability.
 
-            Our intelligent model analyzes soil nutrients, local climate conditions, and market demand to suggest the ideal crop sequence, ensuring sustainable farming practices and maximizing efficiency.
+            <section className={style.hero}>
+                <div className={style.vertical_container}>
+                    <h2>Wellcome to AI-Powered Crop Rotation Advisor </h2>
+                    <div className={style.greenLine}></div>
+                    <h3>Let nature and AI grow your future</h3>
+                </div>
+            </section>
+            <section>
+                <div>
+                <h4>Welcome to our AI-driven crop rotation recommendation system, designed to help farmers optimize their planting strategies for healthier soil, increased yields, and greater profitability.</h4>
 
-            Key Benefits:  Preserve soil health with scientifically optimized crop rotations  Adapt to changing climate conditions for better resilience  Align your crops with market demand to boost profitability
-            Start making smarter, data-driven farming decisions today! </h3>
-    </div>);
+                </div>
+               
+            </section>
+        </div>
+
+    );
 }
 
 export default Home;
