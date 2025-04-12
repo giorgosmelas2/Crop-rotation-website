77602ab7e8dcf3807bc547ec652ee87723382ebe
giorgosmelas2 <giorgosmelas2002@gmail.com>
2025-04-12 19:58:42 +0300

    Rotation Plan started

---
+++ croprotation/src/components/PolygonMap.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from 'react';
+import { EditControl } from 'react-leaflet-draw';
+import { MapContainer, TileLayer, Polygon } from 'react-leaflet';
+import { FeatureGroup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
+
+const PolygonMap = ({ onPolygonCreate }) => {
+    // Function to handle the creation of a polygon
+    // This function is called when the user finishes drawing a polygon on the map
+    const handleCreate = (e) => {
+        const layer = e.layer;
+        const latlngs = layer.getLatLngs()[0];
+        const coords = latlngs.map(p => ({ lat: p.lat, lng: p.lng }));
+        onPolygonCreate(coords);
+    };
+
+    return (
+        <MapContainer
+            center={[39.0742, 21.8243] as [number, number]}
+            zoom={6}
+            style={{ height: '100%', width: '100%' }}>
+            <TileLayer
+                attribution='&copy; OpenStreetMap contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <FeatureGroup>
+                <EditControl
+                    position="topright"
+                    onCreated={handleCreate}
+                    draw={{
+                        rectangle: false,
+                        circle: false,
+                        polyline: false,
+                        circlemarker: false,
+                        marker: false,
+                        polygon: true,
+                    }}
+                />
+            </FeatureGroup>
+        </MapContainer>
+    );
+}
+
+
+
+export default PolygonMap;
--- croprotation/src/pages/RotatioPlan.tsx
+++ croprotation/src/pages/RotatioPlan.tsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { Text } from "../components/Text";
+import style from "../styling/rotationPlan.module.css"
+import handsImage from "../assets/pappous.png"
+import PolygonMap from "../components/PolygonMap";
 
 const RotationPlan = () => {
-    return (<h1>Rotation Plan</h1>);
+    const handlePolygonCreate = (coords) => {
+        console.log('Ο χρήστης σχεδίασε πολύγωνο με συντεταγμένες:', coords);
+      };
+
+    return (
+        <>
+            <section className={style.green}>
+                <div className={style.container}>
+                    <div className={style.box}>
+                        <Text
+                            variant="secondary_title"
+                            color="white"
+                            as="h2">
+                            Tell us about your field, and we’ll tell you what to grow next – smarter soil, happier harvest!
+                        </Text>
+                    </div>
+                    <div className={style.image_box}>
+                        <img src={handsImage} alt="hands" className={style.image} />
+                    </div>
+                </div>
+                <div className={style.calligraphic}>
+                    <Text variant="calligraphic_title"
+                        color="white"
+                        as="h3">
+                        Let’s rotate it right!
+                    </Text>
+                </div>
+            </section>
+            <section className={style.field_info_container}>
+                <div className={style.field_info_box}>
+                    <Text variant="secondary_title"
+                        color="black"
+                        as="h2">
+                        Draw your field on the map.
+                    </Text>
+                    <div className={style.map}>
+                        <PolygonMap onPolygonCreate={handlePolygonCreate} />
+                    </div>
+                </div>
+
+                <div className={style.field_info_box}>
+                <Text variant="secondary_title"
+                        color="black"
+                        as="h2">
+                        Draw your field on the map.
+                    </Text>
+                </div>
+            </section>
+        </>
+    );
 }
 
 
